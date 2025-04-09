9ee15a6ad72f044b88abf1eb390441a312a6f4be
MAHAMAT Annour Mahamat <annour-mahamat.mahamat@niji.fr>
2025-04-09 13:54:35 +0200

    correction url

--- src/components/tests/api.ts
+++ src/components/tests/api.ts
@@ -1,9 +1,8 @@
 // api.ts
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000';
 
 export const fetchScrapedData = async (url: string) => {
-  const response = await axios.post(`${API_URL}/scraping`, { url });
+  const response = await axios.post(`${process.env.VITE_API_URL}/scraping`, { url });
   return response.data;
 };
